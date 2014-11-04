8d1bab1c89fb056c4a4b64530e298850a9c45f1e
Christopher Geary <hello@crgeary.com>
2014-11-04 14:37:43 +0000

    Updated output to fix navbar with depth of 1
    Allow navbar's to be set with a depth of 0. Currently the dropdown classes, caret, and # are all set regardless of the depth (if the depth is 1, then there are no dropdown items to show, so link should work and caret should be hidden).

--- wp_bootstrap_navwalker.php
+++ wp_bootstrap_navwalker.php
@@ -81,7 +81,7 @@ class wp_bootstrap_navwalker extends Walker_Nav_Menu {
 			$atts['rel']    = ! empty( $item->xfn )		? $item->xfn	: '';
 
 			// If item has_children add atts to a.
-			if ( $args->has_children && $depth === 0 ) {
+			if ( $args->has_children && $depth === 0 && $args->depth != 1 ) {
 				$atts['href']   		= '#';
 				$atts['data-toggle']	= 'dropdown';
 				$atts['class']			= 'dropdown-toggle';
@@ -115,7 +115,7 @@ class wp_bootstrap_navwalker extends Walker_Nav_Menu {
 				$item_output .= '<a'. $attributes .'>';
 
 			$item_output .= $args->link_before . apply_filters( 'the_title', $item->title, $item->ID ) . $args->link_after;
-			$item_output .= ( $args->has_children && 0 === $depth ) ? ' <span class="caret"></span></a>' : '</a>';
+			$item_output .= ( $args->has_children && 0 === $depth && $args->depth != 1 ) ? ' <span class="caret"></span></a>' : '</a>';
 			$item_output .= $args->after;
 
 			$output .= apply_filters( 'walker_nav_menu_start_el', $item_output, $item, $depth, $args );
