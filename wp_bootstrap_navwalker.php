6a8e3afa8731b1d73478873f978b051d74920d6d
Webber <rsdntevl@gmail.com>
2017-02-28 21:23:20 -0800

    Fix Nav Link Style for Currently v4.0.0-alpha.6
    v4.0.0-alpha.6 uses .nav-link on the <a></a> tags.
    
    The current php doesn't add the class to the link element. The above change fixes that.

--- wp_bootstrap_navwalker.php
+++ wp_bootstrap_navwalker.php
@@ -90,6 +90,7 @@ class wp_bootstrap_navwalker extends Walker_Nav_Menu {
 				$atts['aria-haspopup']	= 'true';
 			} else {
 				$atts['href'] = ! empty( $item->url ) ? $item->url : '';
+				$atts['class'] = 'nav-link';
 			}
 
 			$atts = apply_filters( 'nav_menu_link_attributes', $atts, $item, $args );
