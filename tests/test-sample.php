cc08db68fab06de6589d4277e9fa951a9195d713
Brandon Hubbard <bkhubbard@gmail.com>
2017-03-07 09:38:47 -0800

    plugin test files

---
+++ tests/test-sample.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Class SampleTest
+ *
+ * @package Wp_Bootstrap_Navwalker
+ */
+
+/**
+ * Sample test case.
+ */
+class SampleTest extends WP_UnitTestCase {
+
+	/**
+	 * A single example test.
+	 */
+	function test_sample() {
+		// Replace this with some actual testing code.
+		$this->assertTrue( true );
+	}
+}
