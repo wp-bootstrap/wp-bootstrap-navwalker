811841fdbcf441f36a600908842af4f0d898538e
Brandon Hubbard <bhubbard@users.noreply.github.com>
2017-03-07 23:43:09 -0800

    Update test-sample.php

--- tests/test-sample.php
+++ tests/test-sample.php
@@ -6,15 +6,38 @@
  */
 
 /**
- * Sample test case.
+ * @group post
+ * @group navmenus
+ * @group taxonomy
+ * @group walker
+ * https://develop.svn.wordpress.org/trunk/tests/phpunit/tests/walker.php
  */
-class SampleTest extends WP_UnitTestCase {
-
-	/**
-	 * A single example test.
-	 */
-	function test_sample() {
-		// Replace this with some actual testing code.
-		$this->assertTrue( true );
+class WP_Test_Bootstrap_NavWalker extends WP_UnitTestCase {
+	
+	function setUp() {
+		$this->walker = new WP_Bootstrap_Navwalker();
+		parent::setUp();
 	}
+	
+	function test_single_item() {
+		$items = array( (object) array( 'id' => 1, 'parent' => 0 ) );
+		$output = $this->walker->walk( $items, 0 );
+		$this->assertEquals( 1, $this->walker->get_number_of_root_elements( $items ) );
+		$this->assertEquals( '<li>1</li>', $output );
+	}
+	
+	function test_single_item_flat() {
+		$items = array( (object) array( 'id' => 1, 'parent' => 0 ) );
+		$output = $this->walker->walk( $items, -1 );
+		$this->assertEquals( 1, $this->walker->get_number_of_root_elements( $items ) );
+		$this->assertEquals( '<li>1</li>', $output );
+	}
+	
+	function test_single_item_depth_1() {
+		$items = array( (object) array( 'id' => 1, 'parent' => 0 ) );
+		$output = $this->walker->walk( $items, 1 );
+		$this->assertEquals( 1, $this->walker->get_number_of_root_elements( $items ) );
+		$this->assertEquals( '<li>1</li>', $output );
+	}
+	
 }
