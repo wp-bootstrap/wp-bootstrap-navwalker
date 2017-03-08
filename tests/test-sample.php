305b1563f8b3339edf59bab38f80c6b00fe8e4d5
Brandon Hubbard <bhubbard@users.noreply.github.com>
2017-03-08 00:21:48 -0800

    Update test-sample.php

--- tests/test-sample.php
+++ tests/test-sample.php
@@ -23,21 +23,7 @@ class WP_Test_Bootstrap_NavWalker extends WP_UnitTestCase {
 		$items = array( (object) array( 'id' => 1, 'parent' => 0 ) );
 		$output = $this->walker->walk( $items, 0 );
 		$this->assertEquals( 1, $this->walker->get_number_of_root_elements( $items ) );
-		$this->assertEquals( '<li itemscope="itemscope" itemtype="https://www.schema.org/SiteNavigationElement" id="" class=""><a title="1" href="#">1</a></li>', $output );
-	}
-	
-	function test_single_item_flat() {
-		$items = array( (object) array( 'id' => 1, 'parent' => 0 ) );
-		$output = $this->walker->walk( $items, -1 );
-		$this->assertEquals( 1, $this->walker->get_number_of_root_elements( $items ) );
-		$this->assertEquals( '<li>1</li>', $output );
-	}
-	
-	function test_single_item_depth_1() {
-		$items = array( (object) array( 'id' => 1, 'parent' => 0 ) );
-		$output = $this->walker->walk( $items, 1 );
-		$this->assertEquals( 1, $this->walker->get_number_of_root_elements( $items ) );
-		$this->assertEquals( '<li>1</li>', $output );
+		$this->assertContains( '<li itemscope="itemscope" itemtype="https://www.schema.org/SiteNavigationElement" id="" class=""><a title="1" href="#">1</a></li>', $output );
 	}
 	
 }
