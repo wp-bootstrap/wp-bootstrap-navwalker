4ecb9e1f6b7dd6804cea751e8d3eaf50cf393df9
Brandon Hubbard <bhubbard@users.noreply.github.com>
2017-03-08 00:35:13 -0800

    Update test-sample.php

--- tests/test-sample.php
+++ tests/test-sample.php
@@ -23,7 +23,7 @@ class WP_Test_Bootstrap_NavWalker extends WP_UnitTestCase {
 		$items = array( (object) array( 'id' => 1, 'parent' => 0 ) );
 		$output = $this->walker->walk( $items, 0 );
 		$this->assertEquals( 1, $this->walker->get_number_of_root_elements( $items ) );
-		$this->assertContains( '<li itemscope="itemscope" itemtype="https://www.schema.org/SiteNavigationElement" id="" class=""><a title="1" href="#">1</a></li>', $output );
+		$this->assertContains( 'https://www.schema.org/SiteNavigationElement', $output );
 	}
 	
 }
