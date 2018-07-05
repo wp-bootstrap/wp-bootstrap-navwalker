a17aa8636a521609e48ac59fcc89c482e7a3236e
Fabio Cagliero <caglierofabio@gmail.com>
2018-07-05 11:50:10 +0200

    Dropdown menus should work when depth is 0

--- class-wp-bootstrap-navwalker.php
+++ class-wp-bootstrap-navwalker.php
@@ -181,7 +181,7 @@ if ( ! class_exists( 'WP_Bootstrap_Navwalker' ) ) {
 			$atts['target'] = ! empty( $item->target ) ? $item->target : '';
 			$atts['rel']    = ! empty( $item->xfn ) ? $item->xfn : '';
 			// If item has_children add atts to <a>.
-			if ( isset( $args->has_children ) && $args->has_children && 0 === $depth && $args->depth > 1 ) {
+			if ( isset( $args->has_children ) && $args->has_children && 0 === $depth && 1 !== $args->depth ) {
 				$atts['href']          = '#';
 				$atts['data-toggle']   = 'dropdown';
 				$atts['aria-haspopup'] = 'true';
