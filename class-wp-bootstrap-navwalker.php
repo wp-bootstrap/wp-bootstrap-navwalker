5d39044bcee8703d1402f7371b033c460b2e0a71
William Patton <will@pattonwebz.com>
2019-10-02 19:15:47 +0100

    Version bump to 4.3.0
    Also removes php 5.3 from travis as it's no longer a supported WP 
    version.

--- class-wp-bootstrap-navwalker.php
+++ class-wp-bootstrap-navwalker.php
@@ -9,7 +9,7 @@
  * Plugin URI:  https://github.com/wp-bootstrap/wp-bootstrap-navwalker
  * Description: A custom WordPress nav walker class to implement the Bootstrap 4 navigation style in a custom theme using the WordPress built in menu manager.
  * Author: Edward McIntyre - @twittem, WP Bootstrap, William Patton - @pattonwebz
- * Version: 4.1.0
+ * Version: 4.3.0
  * Author URI: https://github.com/wp-bootstrap
  * GitHub Plugin URI: https://github.com/wp-bootstrap/wp-bootstrap-navwalker
  * GitHub Branch: master
