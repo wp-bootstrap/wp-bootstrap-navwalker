15661323db81b713391f9f9d1ea6301136ca0469
IanDelMar <42134098+IanDelMar@users.noreply.github.com>
2019-11-08 19:24:12 +0100

    Add rel="noopener noreferrer" to target="_blank"
    Added to class Walker_Nav_Menu in WP 5.2

--- class-wp-bootstrap-navwalker.php
+++ class-wp-bootstrap-navwalker.php
@@ -177,7 +177,11 @@ if ( ! class_exists( 'WP_Bootstrap_Navwalker' ) ) {
 			}
 
 			$atts['target'] = ! empty( $item->target ) ? $item->target : '';
-			$atts['rel']    = ! empty( $item->xfn ) ? $item->xfn : '';
+			if ( '_blank' === $item->target && empty( $item->xfn ) ) {
+            			$atts['rel'] = 'noopener noreferrer';
+        		} else {
+            			$atts['rel'] = $item->xfn;
+        		}
 			// If item has_children add atts to <a>.
 			if ( isset( $args->has_children ) && $args->has_children && 0 === $depth && $args->depth > 1 ) {
 				$atts['href']          = '#';
