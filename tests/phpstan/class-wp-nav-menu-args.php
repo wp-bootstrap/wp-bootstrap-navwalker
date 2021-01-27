c464afb9846e7285525371249f1bce21e903a1a0
IanDelMar <42134098+IanDelMar@users.noreply.github.com>
2021-01-27 18:55:24 +0100

    Add `container_aria_label` argument
    The `container_aria_label` argument has been added in WP 5.5.0.

--- tests/phpstan/class-wp-nav-menu-args.php
+++ tests/phpstan/class-wp-nav-menu-args.php
@@ -56,6 +56,14 @@ class WP_Nav_Menu_Args {
 	public $container_id;
 
 	/**
+	 * The aria-label attribute that is applied to the container
+	 * when it's a nav element. Default empty.
+	 *
+	 * @var string
+	 */
+	public $container_aria_label;
+
+	/**
 	 * If the menu doesn't exists, a callback function will fire.
 	 * Default is 'wp_page_menu'. Set to false for no fallback.
 	 *
